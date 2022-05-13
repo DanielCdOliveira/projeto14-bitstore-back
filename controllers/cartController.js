93206f98769d354796d360ddb98e7bdc4376cecc
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-05-13 14:07:31 -0300

    Fix cartarray insertion

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -13,19 +13,21 @@ export async function addToCart(req, res) {
   console.log(token);
   if (!token) return res.sendStatus(401);
   // trocar para {userId}
-  const session = await db.collection("tokens").findOne({ token });
-  if (!session) return res.sendStatus(401);
+  const {userId}  = await db.collection("tokens").findOne({ token });
+  if (!userId) return res.sendStatus(401);
   try {
     let cartUser = await db
       .collection("carts")
-      .findOne({ userId: session.userId });
+      .findOne({ userId });
     console.log(cartUser);
     if (!cartUser) {
-      let data = { userId: session.userId, cart: [{ ...product, qty: 1 }] };
+      console.log("entrou no !cartuser");
+      let data = { userId, cart: [{ ...product, qty: 1 }] };
       db.collection("carts").insertOne(data);
       console.log(data);
     } else {
       let cartArray = cartUser.cart;
+      let doesExist = false
       cartArray.forEach((element) => {
         if (element._id === product._id) {
           if (plus) {
@@ -33,14 +35,18 @@ export async function addToCart(req, res) {
           } else if (element.qty > 1) {
             element.qty--;
           }
-        } else {
-          let data = { ...product, qty: 1 };
-          cartArray.push(data);
+          doesExist = true
+          return;
         }
       });
+      if(!doesExist){
+        let data = { ...product, qty: 1 };
+        cartArray.push(data);
+      }
+      console.log(cartArray);
       await db
         .collection("carts")
-        .updateOne({ userId: session.userId }, { $set: { cart: cartArray } });
+        .updateOne({ userId }, { $set: { cart: cartArray } });
     }
     res.sendStatus(201);
   } catch (error) {}
